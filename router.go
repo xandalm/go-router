18236ba0bb8ea07504f3ed8546eb61ee554969a4
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-12-28 16:18:36 -0300

    add test to UseFunc feature

--- router.go
+++ router.go
@@ -6,10 +6,27 @@ type RouteHandler interface {
 	ServeHTTP(http.ResponseWriter, *http.Request)
 }
 
+type RouteHandlerFunc func(http.ResponseWriter, *http.Request)
+
+func (f RouteHandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	f(w, r)
+}
+
 type Router struct {
 	m map[string]RouteHandler
 }
 
+func (ro *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	path := r.URL.Path
+
+	h := ro.m[path]
+
+	h.ServeHTTP(w, r)
+}
+
+// Records the given pattern and handler to handle the corresponding path.
+// Use is a generic method correspondent
 func (ro *Router) Use(pattern string, handler RouteHandler) {
 	if pattern == "" {
 		panic("router: invalid pattern")
@@ -25,11 +42,11 @@ func (ro *Router) Use(pattern string, handler RouteHandler) {
 	ro.m[pattern] = handler
 }
 
-func (ro *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	path := r.URL.Path
-
-	h := ro.m[path]
-
-	h.ServeHTTP(w, r)
+// Instead Use method, this method get a handler as a func.
+// And wrap it, to act like a RouteHandler.
+func (ro *Router) UseFunc(pattern string, handler func(w http.ResponseWriter, r *http.Request)) {
+	if handler == nil {
+		panic("router: nil handler")
+	}
+	ro.Use(pattern, RouteHandlerFunc(handler))
 }
--- router_test.go
+++ router_test.go
@@ -55,5 +55,50 @@ func TestUse(t *testing.T) {
 			t.Errorf("got handler %+v, but want %+v", handler, dummyHandler)
 		}
 	})
+}
 
+func TestUseFunc(t *testing.T) {
+
+	t.Run("panic on empty pattern", func(t *testing.T) {
+		router := &Router{}
+
+		defer func() {
+			r := recover()
+			if r == nil {
+				t.Error("didn't panic")
+			}
+		}()
+
+		dummyHandler := func(w http.ResponseWriter, r *http.Request) {
+		}
+
+		router.UseFunc("", dummyHandler)
+	})
+
+	t.Run("panic on nil handler", func(t *testing.T) {
+		router := &Router{}
+
+		defer func() {
+			r := recover()
+			if r == nil {
+				t.Error("didn't panic")
+			}
+		}()
+
+		router.UseFunc("/path", nil)
+	})
+
+	t.Run("add /user pattern", func(t *testing.T) {
+		router := &Router{}
+
+		dummyHandler := func(w http.ResponseWriter, r *http.Request) {
+		}
+
+		router.UseFunc("/user", dummyHandler)
+
+		_, ok := router.m["/user"]
+		if !ok {
+			t.Errorf("not registered the pattern")
+		}
+	})
 }
